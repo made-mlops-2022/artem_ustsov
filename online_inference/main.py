fe51c3d9468faf457494221b26addddf6773dcf4
Artem Ustsov <a.ustsov@vk.team>
2022-11-14 01:14:12 +0300

    api, schema, docker, requests are ready

--- online_inference/main.py
+++ online_inference/main.py
@@ -7,9 +7,59 @@ import pandas as pd
 from fastapi import FastAPI
 from fastapi_health import health
 
-from schemas import MedicalFeatures
+from schema import MedicalFeatures
 
 app = FastAPI()
-
 model = None
-raw_transformer = None
+
+
+@app.on_event('startup')
+def load_model():
+    """Predefines. Upload the model"""
+
+    path_to_model = os.getenv('PATH_TO_MODEL')
+    with open(path_to_model, 'rb') as f:
+        global model
+        model = pickle.load(f)
+
+
+@app.get('/')
+def home():
+    """Home page"""
+
+    return {"key": "Hello"}
+
+
+@app.post('/predict')
+async def predict(data: MedicalFeatures):
+    """Get the data from request.
+    Make prediction and send response to user
+    """
+
+    data_df = pd.DataFrame([data.dict()])
+    y = model.predict(data_df)
+    condition = 'healthy' if not y[0] else 'sick'
+
+    return {'condition': condition}
+
+
+def check_ready():
+    """General model check"""
+
+    return model is not None
+
+
+async def success_handler(**kwargs):
+    """Success health check"""
+
+    return 'Model is ready'
+
+
+async def failure_handler(**kwargs):
+    """Failure health check"""
+
+    return 'Model is not ready'
+
+app.add_api_route('/health', health([check_ready],
+                  success_handler=success_handler,
+                  failure_handler=failure_handler))
