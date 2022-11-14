8eb575b16d2d04a72222a9ebea15f8a6350e7fef
Artem Ustsov <a.ustsov@vk.team>
2022-11-14 20:07:37 +0300

    rebuild

--- ml_project/ml_project/data/load_data.py
+++ ml_project/ml_project/data/load_data.py
@@ -6,9 +6,8 @@ import pandas as pd
 import boto3
 from sklearn.model_selection import train_test_split
 
-from ml_project.entities.split_params import SplittingParams
+from ml_project_.ml_project.entities.split_params import SplittingParams
 import logging
-import os
 
 
 def download_data_from_s3(
